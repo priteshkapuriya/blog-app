18fd47d58f1594d878018dd23c7180128cb8c879
priteshkapuriya <34333521+priteshkapuriya@users.noreply.github.com>
2022-08-15 14:09:56 +0530

    Removing log statements

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 export const AppBar = ({ pages }) => {
-  console.log("Pritesh", pages);
   const [isShow, setShow] = useState(false);
   const navigate = useNavigate();
   const { user, logout } = useAuth();
